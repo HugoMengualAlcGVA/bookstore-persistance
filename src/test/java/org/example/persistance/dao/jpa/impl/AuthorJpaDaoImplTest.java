db975241d9002554d77ca68d1c81730ddf2a969e
HugoMengualAlcGVA <hugmenalc@alu.edu.gva>
2025-11-24 09:13:10 +0100

    test(), count y findBySlug

--- src/test/java/org/example/persistance/dao/jpa/impl/AuthorJpaDaoImplTest.java
+++ src/test/java/org/example/persistance/dao/jpa/impl/AuthorJpaDaoImplTest.java
@@ -1,19 +1,33 @@
 package org.example.persistance.dao.jpa.impl;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.example.persistance.TestConfig;
 import org.example.persistance.dao.jpa.AuthorJpaDao;
 import org.example.persistance.dao.jpa.entity.AuthorJpaEntity;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ContextConfiguration;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
+@ContextConfiguration(classes = TestConfig.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class AuthorJpaDaoImplTest {
 
-    private final AuthorJpaDao authorJpaDao = new AuthorJpaDaoImpl();
+    //private final AuthorJpaDao authorJpaDao = new AuthorJpaDaoImpl();
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Autowired
+    private AuthorJpaDao authorJpaDao;
 
     @Test
     @DisplayName("Insert a new author and verify it is saved correctly")
@@ -55,11 +69,37 @@ class AuthorJpaDaoImplTest {
         ));
 
     }
-    
+
     @Test
-    @DisplayName("")
+    @DisplayName("Test count() does in fact count properly")
+    void countTest(){
+        String sql = "SELECT COUNT(b) FROM AuthorJpaEntity b";
+        long expectedCount = entityManager.createQuery(sql, Long.class)
+                .getSingleResult();
+        long actualCount = authorJpaDao.count();
+
+        assertEquals(expectedCount, actualCount);
+    }
+
+    @Test
+    @DisplayName("Test findBySlug returns whats expected")
     void findBySlugTest(){
-        
+        String sql = "SELECT TOP 1 a FROM AuthorJpaEntity a";
+        AuthorJpaEntity existingAuthor = entityManager.find(AuthorJpaEntity.class, sql);
+
+        Optional<AuthorJpaEntity> response = authorJpaDao.findBySlug(existingAuthor.getSlug());
+
+        assertNotNull(response, "Returned Author entity should not be null");
+        response.ifPresent(authorFound -> assertAll(
+                () -> assertEquals(existingAuthor.getId(), authorFound.getId()),
+                () -> assertEquals(existingAuthor.getName(), authorFound.getName()),
+                () -> assertEquals(existingAuthor.getSlug(), authorFound.getSlug()),
+                () -> assertEquals(existingAuthor.getNationality(), authorFound.getNationality()),
+                () -> assertEquals(existingAuthor.getBiographyEs(), authorFound.getBiographyEs()),
+                () -> assertEquals(existingAuthor.getBiographyEn(), authorFound.getBiographyEn()),
+                () -> assertEquals(existingAuthor.getBirthYear(), authorFound.getBirthYear()),
+                () -> assertEquals(existingAuthor.getDeathYear(), authorFound.getDeathYear())
+        ));
     }
 
 }
